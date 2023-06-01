7866abe596251a97c27ad55a306e9d5b3c66482a
baby5 <zxnzysj@163.com>
2023-06-01 23:11:23 +0800

    Add TwoSum

---
+++ JavaScript/0001_TwoSum.js
@@ -0,0 +1,14 @@
+// https://leetcode.cn/problems/two-sum/
+
+var twoSum = function(nums, target) {
+	var obj = {}
+	for (var i = 0; i < nums.length; i++) {
+		var num = nums[i]
+		var j = obj[target - num]
+		if (j !== undefined) {
+			return [j, i]
+		}
+		obj[num] = i
+	}
+	return []
+};

186d5bd439e69c9425997311c8d9aded6e047788
baby5 <zxnzysj@163.com>
2023-06-10 17:47:19 +0800

    Update 0001

--- JavaScript/0001_TwoSum.js
+++ JavaScript/0001_TwoSum.js
@@ -1,6 +1,6 @@
 // https://leetcode.cn/problems/two-sum/
 
-var twoSum = function(nums, target) {
+var twoSum = function(nums, target) {  // TC: O(n) SC: O(n)
 	var obj = {}
 	for (var i = 0; i < nums.length; i++) {
 		var num = nums[i]
@@ -9,6 +9,19 @@ var twoSum = function(nums, target) {
 			return [j, i]
 		}
 		obj[num] = i
+	}
+	return []
+};
+
+var twoSum_2 = function(nums, target) {  // TC: O(n²) SC: O(1)
+	var len = nums.length
+	for (var i = 0; i < len - 1; i++) {
+		var num = nums[i]
+		for (var j = i+1; j < len; j++) {
+			if (num + nums[j] === target) {
+				return [i, j]
+			}
+		}
 	}
 	return []
 };

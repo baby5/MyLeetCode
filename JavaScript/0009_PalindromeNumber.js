9fd5dcfff04f1cdad4f78488551363705aa4f3aa
baby5 <zxnzysj@163.com>
2023-06-10 17:09:41 +0800

    Add 0009

---
+++ JavaScript/0009_PalindromeNumber.js
@@ -0,0 +1,18 @@
+// https://leetcode.cn/problems/palindrome-number/
+
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false
+    }
+
+    var x_str = x.toString()
+    var len = x_str.length
+    for (var i = 0; i < Math.floor(len / 2); i++) {
+        if (x_str[i] !== x_str[len - i - 1]) {
+            return false
+        }
+    }
+
+    return true
+};
+
